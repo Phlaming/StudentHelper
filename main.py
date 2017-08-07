43e649eb717f3ce3c66956a92cc585b79ee107a2
Mason Emura <memura@csumb.edu>
2017-08-07 16:27:04 -0700

    Added Jinja2

--- main.py
+++ main.py
@@ -15,6 +15,10 @@
 # limitations under the License.
 #
 import webapp2
+import jinja2
+import os
+
+jinja_environment = jinja2.Environment(loader=jinja2.FileSystemLoader(os.path.dirname(__file__)))
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
