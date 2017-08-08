bc2e017e6b121dcba6b6e67963b84c7f94666ab3
Juan Trinidad <jtrinidad-luis@csumb.edu>
2017-08-07 17:05:51 -0700

    Working Login

--- main.py
+++ main.py
@@ -17,13 +17,24 @@
 import webapp2
 import jinja2
 import os
+from google.appengine.api import users
 
 jinja_environment = jinja2.Environment(loader=jinja2.FileSystemLoader(os.path.dirname(__file__)))
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        template = jinja_environment.get_template('templates/log_in.html')
-        self.response.write(template.render())
+        user = users.get_current_user()
+        if user:
+            nickname = user.nickname()
+            logout_url = users.create_logout_url('/')
+            greeting = 'Welcome, {}! (<a href="{}">sign out</a>)'.format(
+                nickname, logout_url)
+        else:
+            login_url = users.create_login_url('/')
+            greeting = '<a href="{}">Sign in</a>'.format(login_url)
+
+        self.response.write(
+            '<html><body>{}</body></html>'.format(greeting))
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
