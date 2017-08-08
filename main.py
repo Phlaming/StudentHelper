800689d684426a0de8b94a45f13862282bfe7cc7
abuyck98 <abuyck@csumb.edu>
2017-08-08 10:37:50 -0700

    Changed

--- main.py
+++ main.py
@@ -23,18 +23,20 @@ jinja_environment = jinja2.Environment(loader=jinja2.FileSystemLoader(os.path.di
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
+        template = jinja_environment.get_template('templates/log_in.html')
         user = users.get_current_user()
         if user:
             nickname = user.nickname()
             logout_url = users.create_logout_url('/')
-            greeting = 'Welcome, {}! (<a href="{}">sign out</a>)'.format(
+            greeting = """Welcome, {}! (<a href='{}'>sign out</a>)""".format(
                 nickname, logout_url)
         else:
             login_url = users.create_login_url('/')
-            greeting = '<a href="{}">Sign in</a>'.format(login_url)
+            greeting = """<a href="{}">Sign in</a>""".format(login_url)
 
         self.response.write(
-            '<html><body>{}</body></html>'.format(greeting))
+            """<html><body>{}</body></html>""".format(greeting))
+        self.response.write(template.render())
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
